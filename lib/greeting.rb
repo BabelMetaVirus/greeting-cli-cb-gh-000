458c53c8d5ac814fc2a99fb0d40bca5a8fb59a9a
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-18 20:40:53 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,5 +3,5 @@
 # greeting: takes in a string for a user name and outputs
 #           a fixed string including that name.
 def greeting(name)
-  puts("Hello #{name}. It's nice to meet you.")
+  puts("Hello #{name.strip}. It's nice to meet you.")
 end
